d115ce1987355eecad8249280ffe13c382a85d4b
vshivaswamy <vinnetus@gmail.com>
2016-02-28 17:02:22 -0600

    added programming assignments 3

---
+++ ProgrammingAssignment3/best.R
@@ -0,0 +1,39 @@
+best <- function(state, outcome) {
+  ## Read outcome data
+  fileName <- "outcome-of-care-measures.csv"
+  data <- read.csv(fileName, colClasses="character")
+  # change data type from character to numeric
+  data[, 11] <- as.numeric(data[, 11]) # heart attack
+  data[, 17] <- as.numeric(data[, 17]) # heart failure
+  data[, 23] <- as.numeric(data[, 23]) # pneumonia
+  
+  ## Check that state and outcome are valid
+  valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
+  if (!state %in% data$State) {
+    stop("invalid state")
+  } else if(!outcome %in% valid_outcomes) {
+    stop("invalid outcome")
+  } else {
+    if(outcome == "heart attack") {
+      hosp_name <- findHospitalName(data, 11, state)
+    } else if(outcome == "heart failure") {
+      hosp_name <- findHospitalName(data, 17, state)
+    } else {
+      hosp_name <- findHospitalName(data, 23, state)
+    }
+    result <- hosp_name
+    return(result)
+  }
+  
+  
+  
+}
+
+findHospitalName <- function(data, col_num, state) {
+  state_subset <- data[data[, 7]==state, ]
+  outcome_arr <- state_subset[, col_num]
+  min <- min(outcome_arr, na.rm=T)
+  min_index <- which(outcome_arr == min)
+  hosp_name <- state_subset[min_index, 2]
+  return(hosp_name)
+}
